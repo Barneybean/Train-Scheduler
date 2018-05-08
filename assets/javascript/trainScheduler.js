816bc7645f94b9e5cad733416cdbbb7858e2ff11
William <William@MacBook-Pro-2.local>
2018-05-08 09:39:29 -0700

    added .gitignore

---
+++ assets/javascript/trainScheduler.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAnJ1RX2SSICm_VeXyQBMznuG33V5xvvDE",
+    authDomain: "trainscheduler-fa676.firebaseapp.com",
+    databaseURL: "https://trainscheduler-fa676.firebaseio.com",
+    projectId: "trainscheduler-fa676",
+    storageBucket: "",
+    messagingSenderId: "434641081543"
+  };
+  firebase.initializeApp(config);
